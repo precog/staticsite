b9cea66390fa19104b1af620291c5ff05c0a5dae
Matthew T De Goes <matthew@reportgrid.com>
2013-04-17 15:31:23 -0600

    Fix for Reset Password

--- contents/js/unique.js
+++ contents/js/unique.js
@@ -563,13 +563,24 @@ $(document).ready(function(){
                   
                   if (userEmail) {
                         $.getScript("/js/precog.js", function(){
-                              Precog.$.Config.analyticsService = "https://beta.precog.com/";
+                              findAccount(userEmail,
+                                    function(serviceUrl) {
+                                          
+                                          Precog.$.Config.analyticsService = serviceUrl;
                               
-                              Precog.requestResetPassword(userEmail, function(){
-                                    $("#precog-form-login").append("<div id='form-success'><p class='success-font'>A reset link has been sent to your e-mail.</p></div>").find("#form-success").delay(2000).fadeOut(500);
-                              }, function(){
-                                    $("#precog-form-login").append("<div id='form-error'><p class='error-font'>The email address you have entered is not valid. Please try again.</p></div>").find("#form-error").delay(2000).fadeOut(500);
-                              });
+                                          Precog.requestResetPassword(userEmail, function(){
+                                                $("#precog-form-login").append("<div id='form-success'><p class='success-font'>A reset link has been sent to your e-mail.</p></div>").find("#form-success").delay(2000).fadeOut(500);
+                                          }, function(){
+                                                $("#precog-form-login").append("<div id='form-error'><p class='error-font'>The email address you have entered is not valid. Please try again.</p></div>").find("#form-error").delay(2000).fadeOut(500);
+                                          });
+                                          
+                                    },
+                                    function() {
+                                          $("#precog-form-login").append("<div id='form-error'><p class='error-font'>We were unable to find your account, please double check your enter email address.</p></div>").find("#form-error").delay(2000).fadeOut(500);
+                                          // USER HAS NO ACCOUNT ANYWHERE!!! MAYBE EMAIL IS WRONG???
+                                    }
+                              );
+                              
                         });
                   } else {
                         $("#precog-form-login").append("<div id='form-error'><p class='error-font'>Please enter an email address.</p></div>").find("#form-error").delay(2000).fadeOut(500);
