21a28aa95b851a5a1180b300e5c32f2719e3e338
Matthew T De Goes <matthew@reportgrid.com>
2013-04-17 15:53:23 -0600

    Revert "Fix for Reset Password"
    This reverts commit e34c8fd9fb854d28a8e5923f6b0357351597201c.

--- contents/js/unique.js
+++ contents/js/unique.js
@@ -563,9 +563,8 @@ $(document).ready(function(){
                   
                   if (userEmail) {
                         $.getScript("/js/precog.js", function(){
+                              Precog.$.Config.analyticsService = "https://beta.precog.com/";
                               
-                              Precog.$.Config.analyticsService = "http://beta.precog.com/";
-                                    
                               Precog.requestResetPassword(userEmail, function(){
                                     $("#precog-form-login").append("<div id='form-success'><p class='success-font'>A reset link has been sent to your e-mail.</p></div>").find("#form-success").delay(2000).fadeOut(500);
                               }, function(){
