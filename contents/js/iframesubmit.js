528eab988ca6ceefd5a530f98f26ec3eea496b6c
Matthew T De Goes <matthew@reportgrid.com>
2013-04-19 14:08:46 -0600

    Update to the Pardot Script

--- contents/js/iframesubmit.js
+++ contents/js/iframesubmit.js
@@ -1,75 +1,39 @@
-define([
-  // TODO needs cookie for email
-    "app/util/iframesubmit"
-  , "app/util/dialog-support"
-  , "libs/jquery/jquery.cookie/jquery.cookie"
-],
-
-function(submit, displaySupport) {
-  var PAGE_ACTION = "https://labcoat.precog.com/actions/",
-      FORM_ACTION = "http://www2.precog.com/l/",
-      wrapper,
-      queue = [],
-      email = $.cookie("Precog_eMail"),
-      action_map = {
-        "quirrel_failure_default" : "17892/2013-01-14/29k3q",
-        "quirrel_failure_custom"  : "17892/2013-01-14/29k52",
-        "generic_error"           : "17892/2013-01-14/29k5d"
-      };
-
-  var onprecog = Precog.$.Config.analyticsService.indexOf(".precog.com") >= 0;
-
-  if(onprecog) {
-    wrapper = {
-      track_page : function(action) {
-        submit({
-          action : PAGE_ACTION + (action_map[action] || action),
-          method : "get",
-          complete : function() {
-//          console.log("Page Action Done: " + PAGE_ACTION + action);
-          }
-        });
-      }
-    }
-  } else {
-    wrapper = {
-      track_page : function(action) { }
-    }
-  }
-  wrapper.track_error = function(action, params, user_message) {
-    params = params || {};
-    if(!email) {
-      displaySupport("report error", user_message, null, params.error_message, false, function(e, r) {
-        email = e;
-        $.cookie("Precog_eMail", email);
-        params.error_message = r;
-        wrapper.track_error(action, params, user_message);
-      });
-      return false;
-    } else {
-      params.email = email;
-      submit({
-        action : FORM_ACTION + (action_map[action] || action),
-        method : "post",
-        data : params,
-        complete : function() {
-//            console.log("Form Submit: " + action);
+$.iframeSubmit = function(method) {
+  var uid = "ifr_"+Math.floor(Math.random() * 214783647),
+    settings = {
+      action : "",
+      method : "post",
+      window : window,
+      data   : {},
+      complete : function() {},
+      remove_after : 10000
+    },
+    methods = {
+      init : function(options) {
+        settings = $.extend(settings, options);
+        var $iframe = $('<iframe name="'+uid+'" style="display:none"></iframe>').appendTo("body"),
+            $form   = $('<form action="'+settings.action+'" method="'+settings.method.toUpperCase()+'" target="'+uid+'" style="display:none"></form>').appendTo("body");
+        for(var key in settings.data) {
+          if(!settings.data.hasOwnProperty(key)) continue;
+          $('<input type="hidden" name="'+key+'" value="'+settings.data[key]+'">').appendTo($form);
         }
-      });
-      return true;
-    }
-  };
-
-  wrapper.submit_form = function(url, params, callback) {
-    submit({
-      action : url,
-      method : "get",
-      data : params,
-      complete : function() {
-        if(callback) callback();
+        $iframe.on("load", function() {
+          $form.remove();
+          setTimeout(function() {
+            $iframe.remove();
+          }, settings.remove_after);
+          settings.complete();
+        });
+        $form.submit();
+        return this;
       }
-    });
-  };
+    };
 
-  return wrapper;
-});
+  if ( methods[method] ) {
+    return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+  } else if ( typeof method === 'object' || ! method ) {
+    return methods.init.apply( this, arguments );
+  } else {
+    $.error( 'Method ' +  method + ' does not exist on jQuery.iframeSubmit' );
+  }
+};
