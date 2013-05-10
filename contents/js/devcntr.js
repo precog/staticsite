3028836e06f1ebd45546cbbfa1e6bae4802ad0a3
Matthew De Goes <matthew@precog.com>
2013-05-10 12:44:16 -0600

    Tool Tip Fix and Disable Smooth Scroll for Testing.

--- contents/js/devcntr.js
+++ contents/js/devcntr.js
@@ -16,24 +16,18 @@ $(document).ready(function(){
     //API KEY
     $("#page-content p, #page-content ul li").each(function(){
         var html = $(this).html();
-        var newHtml = html.replace(/API Key|API key|api key|Api key|Api Key/gi, '<span id="api-key" class="tt-link">API Key</span>')
-        .replace(/Root Path|Root path|RootPath|ROOT Path/gi, '<span id="root-path" class="tt-link">Root Path</span>')
-        .replace(/Account ID|Account id|Account Id/gi, '<span id="account-id" class="tt-link">Account ID</span>')
-        .replace(/PATH|Path|path/gi, '<span id="path" class="tt-link">Path</span>')
-        .replace(/grant-id|Grant-id|Grant-Id|Grant-ID/gi, '<span id="grant-id" class="tt-link">Grant ID</span>')
-        .replace(/auth api key|Auth Api Key|Auth API key/gi, '<span id="auth-api-key" class="tt-link">Auth API Key</span>')
-        .replace(/Query|query|QUERY/gi, '<span id="query" class="tt-link">Query</span>');
+        var newHtml = html.replace(/API Key|API key|api key|Api key|Api Key/gi, '<span id="api-key" class="tt-link">API Key</span>').replace(/Root Path|Root path|RootPath|ROOT Path/gi, '<span id="root-path" class="tt-link">Root Path</span>').replace(/Account ID|Account id|Account Id/gi, '<span id="account-id" class="tt-link">Account ID</span>').replace(/PATH|Path|path/gi, '<span id="path" class="tt-link">Path</span>').replace(/grant-id|Grant-id|Grant-Id|Grant-ID/gi, '<span id="grant-id" class="tt-link">Grant ID</span>').replace(/auth api key|Auth Api Key|Auth API key/gi, '<span id="auth-api-key" class="tt-link">Auth API Key</span>').replace(/Query|query|QUERY/gi, '<span id="query" class="tt-link">Query</span>');
         
         $(this).html(newHtml);
     });
     
     //TEXT TOOL TIP FUNCTION
-    $("#api-key, #root-path, #account-id").mouseenter(function(){
+    $(".tt-link").mouseenter(function(){
         var currentTip = $(this).attr("id");
         var currentPosition = $(this).offset();
         var leftAdjustment = 600 - ($(this).width());
         
-        var panelHeight = ($("#" + currentTip + "-tt").height()) + 110;
+        var panelHeight = ($("#" + currentTip + "-tt").height()) + 120;
 
         $("#" + currentTip + "-tt").css({
             left: currentPosition.left -leftAdjustment,
@@ -58,13 +52,13 @@ $(document).ready(function(){
     });
     
     //BODY LINK SMOOTH SCROLL
-    $("#section-index").on("click", "a", function(e){
+    /*$("#section-index").on("click", "a", function(e){
         e.preventDefault();
         
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top - 50
         }, 500);
-    });
+    });*/
     
     //UL DETECT
     $("ul li").each(function(){
@@ -72,8 +66,6 @@ $(document).ready(function(){
         
         if (currentHTML == "JSON") {
             $(this).parent("ul").addClass("rest-request");
-            
-            console.log(this);
         }
     });
     
