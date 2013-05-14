9653e29f9ffa76cf92db7eba9d7c7a06dc032b6c
Matthew De Goes <matthew@precog.com>
2013-05-14 10:44:42 -0600

    Changes Reversion

--- contents/js/unique.js
+++ contents/js/unique.js
@@ -203,27 +203,8 @@ $(document).ready(function(){
                                                             sessionStorage.setItem('PrecogAccount_BasePath', additionalAccountDetails.rootPath);
                                                             sessionStorage.setItem('PrecogAccount_Login', 'Logged In');
                                                             
-                                                            var firstName = userName.split(' ').slice(0, -1).join(' ');
-                                                            var lastName = userName.split(' ').slice(-1).join(' ');
                                                             
-                                                            var url = ("https://app.icontact.com/icp/a/1348845/c/1093/");
-                                                            
-                                                            var customerData = {
-                                                            "email": userEmail,
-                                                            "firstName": firstName,
-                                                            "lastName": lastName
-                                                            };
-                                                                
-                                                            $.ajax({
-                                                                  type: "POST",
-                                                                  url: url,
-                                                                  data: customerData,
-                                                                  success: function(){
-                                                                      //On Success Re Route window.location = "/account/"
-                                                                      console.log("Data Submitted");
-                                                                  },
-                                                                  error: function(err){ console.log('my message', err); }
-                                                            });
+                                                            window.location = "/account/"
                                                             
                                                       });
                                                       
