4bc98f34fbb99f96245ca5e689364963b6b9ce66
Matthew De Goes <matthew@precog.com>
2013-05-16 14:39:11 -0600

    Additional Styling + Some Obfuscation

--- contents/external/verizon-demo/js/default.js
+++ contents/external/verizon-demo/js/default.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
 
   //BUBBLE CHART HOME PAGE
   $("#custom-bubble-vis span").each(function(){
-    var elemVar = $(this).html() / 110;
+    var elemVar = $(this).html() / 120;
     var textVar = elemVar / 5;
     
     $(this).css({width: elemVar, height: elemVar, fontSize: textVar, borderRadius: elemVar, lineHeight: (elemVar + "px")})
     console.log(elemVar);
   });
+  
   
 });
