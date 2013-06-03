1d2411019049eb86a6be1e432edbc4bb4408d30e
Matthew De Goes <matthew@precog.com>
2013-06-03 15:25:44 -0600

    Security Update for IContact Sync

--- nodejs/routes/icontact.js
+++ nodejs/routes/icontact.js
@@ -13,7 +13,7 @@ function makeRequest(method, path, handler) {
             'Api-Version': '2.0',
             'Api-AppId': 'KDCT1QfMCjSa3nAXk4xE7KjIBzJwqSxi',
             'Api-Username': 'matthew@precog.com',
-            'Api-Password': '4Contact'
+            'Api-Password': 'PrecogContacts'
         }
     };
 
